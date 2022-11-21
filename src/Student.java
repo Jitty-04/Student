e1104d851b5fbdd928ef161331a4cf34b7d809d1
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-22 00:30:13 +0530

    p

--- src/Student.java
+++ src/Student.java
@@ -26,6 +26,29 @@ public class Student {
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             switch (n) {
+                case 1:
+                    System.out.println("Enter name");
+                    String name = sc.next();
+                    System.out.println("Enter Roll number ");
+                    int roll = sc.nextInt();
+                    System.out.println("Enter Admission number");
+                    String admn = sc.next();
+                    System.out.println("Enter College");
+                    String college = sc.next();
+                    Stud e = new Stud(name, roll, admn, college);
+                    arr.add(e);
+                    break;
+                case 2 :
+                    for (Stud emp : arr) {
+                        System.out.println("Printing details of Student " );
+                        System.out.println(emp.name);
+                        System.out.println(emp.rollno);
+                        System.out.println(emp.admnno);
+                        System.out.println(emp.college);
+                    }
+                    break;
+
+
             }
         }
     }
