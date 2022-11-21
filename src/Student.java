7b427b56a52b502b0dddd08adfdb8b481d211231
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-21 23:39:39 +0530

    p2

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,32 @@
-public class Student {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+class Stud {
+    String name;
+    int rollno;
+    String admnno;
+    String college;
+
+    public Stud(String name, int rollno, String admnno, String college) {
+        this.name = name;
+        this.rollno = rollno;
+        this.admnno = admnno;
+        this.college = college;
+    }
+}
+public class Student {
+    public static void main(String[] args) {
+        ArrayList<Stud> arr = new ArrayList<Stud>();
+        while (true) {
+            System.out.println("1 - Add Employee");
+            System.out.println("2 - View Employee");
+            System.out.println("3 - Search an Employee");
+            System.out.println("4 - Delete an Employee");
+            System.out.println("5 - Exit");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            switch (n) {
+            }
+        }
+    }
 }
